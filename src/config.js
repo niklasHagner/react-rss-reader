7ad388d6751d68e3299b41fa7a0a773385e2a9cb
niklasHagner <niklas.hagner@outlook.com>
2020-09-13 15:45:39 +0200

    change cache mode from forever to true

--- src/config.js
+++ src/config.js
@@ -3,7 +3,7 @@
 const ONE_HOUR = 60 * 60 * 1000;
 
 const config = {
-    cache: { mode: "forever", time: ONE_HOUR*999 }
+    cache: { mode: "true", time: ONE_HOUR*999 }
 }
 
 export default config;
