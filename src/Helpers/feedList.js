09a6521da15a53f2901a0ae844a81b9e4f1384c5
niklasHagner <niklas.hagner@outlook.com>
2021-08-19 21:18:36 +0200

    Delete dead feeds

--- src/Helpers/feedList.js
+++ src/Helpers/feedList.js
@@ -9,21 +9,15 @@ var feeds = [
   { url: 'https://www.smashingmagazine.com/feed/', color: '#e95c33' },
   { url: 'http://feeds.feedburner.com/CssTricks', color: 'rgb(255, 152, 0)' },
   { url: 'http://feeds.feedburner.com/Devlounge', color: 'dodgerblue' },
-  // { url: 'http://dn.se/rss', color: 'rgb(244, 0, 14)' },
-  // { url: 'https://feeds.expressen.se/nyheter/', color: 'darkblue' },
   { url: 'https://hacks.mozilla.org/feed/', color: 'orangered' },
-  { url: 'http://feeds.feedburner.com/456bereastreet', color: 'violet' },
   { url: 'http://feeds.feedburner.com/FunctioningForm', color: 'pink' },
   { url: 'https://javascriptweblog.wordpress.com/feed/', color: '#900C3F' },
   { url: 'https://code-cartoons.com/feed', color: 'blue' },
-  { url: 'https://toddmotto.com/feed.xml', color: 'purple' },
   { url: 'https://www.joelonsoftware.com/feed/', color: 'gold' },
-  { url: 'https://feeds.feedburner.com/codinghorror', color: 'magenta' },
-  { url: 'https://feeds.feedburner.com/codinghorror', color: '#ccc' },
+  { url: 'https://blog.codinghorror.com/rss/', color: 'magenta' },
   { url: 'http://feeds.feedburner.com/37signals/beMH', color: '#999' },
   { url: 'http://steve-yegge.blogspot.com/feeds/posts/default', color: '#568be1' },
-  { url: 'https://www.feedspot.com/infiniterss.php?q=site:http%3A%2F%2Fwww.thecrazyprogrammer.com%2Ffeed', color: 'cyan' },
-  { url: 'https://www.feedspot.com/infiniterss.php?q=site:https%3A%2F%2Fwww.reddit.com%2Fr%2Fprogramming%2F.rss', color: '#a7e156' },
+  { url: 'https://www.reddit.com/r/programming/.rss', color: '#a7e156' },
 ];
 
 const apiKey = getApiKey();
